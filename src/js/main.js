26374957c0dc616c320678e467f0f300acd6e994
Murad Karimov <muradkarimov07@gmail.com>
2024-01-23 23:51:02 +0300

    create effects.js file

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ $('div').click(function() {
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closest('.findme'));
-console.log($('.findme').siblings());
+// console.log($('.findme').siblings());
+$('button').fadeIn(1800);
 
 // console.log($('button').html('Hello'));
